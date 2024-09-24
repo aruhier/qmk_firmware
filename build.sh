cee996457ef69787095861a4c5c4eef84ce3991f
Anthony Ruhier <aruhier@mailbox.org>
2024-09-24 19:52:59 +0200

    Update submodules before building/flashing

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+qmk git-submodule
 qmk compile --keyboard gmmk/pro/rev1/iso -km aruhier
--- flash.sh
+++ flash.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+qmk git-submodule
 qmk flash --keyboard gmmk/pro/rev1/iso -km aruhier
